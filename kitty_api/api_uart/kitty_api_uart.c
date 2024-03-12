f2eabb136a9772cbab9f97b6c57440f76e8d42d8
Butaforsky <59537160+Butaforsky@users.noreply.github.com>
2024-03-13 02:39:43 +0300

    upd

---
+++ kitty_api/api_uart/kitty_api_uart.c
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @brief Uart driver for kitty
+ * @author butaforsky
+ * @version 0.1.0
+ */
+
+#include "kitty_api_uart.h"
+#include "short_types.h"
+
+/* User defines */
+
+/* User enums */
+
+/* User structs */
+
+/* User typedefs */
+
+/* User variables */
+
+/* User functions */
+
+
+
+
+
+
+
---
+++ kitty_api/api_uart/kitty_api_uart.h
@@ -0,0 +1,32 @@
+#ifndef __KITTY_API_UART_H__
+#define __KITTY_API_UART_H__
+
+/**
+ * 
+ * @brief Uart driver for kitty * 
+ * @author butaforsky
+ * @version 0.1.0
+ */
+
+#include "main.h"
+#include "short_types.h"
+
+/* User defines */
+
+/* User enums */
+
+/* User structs */
+
+/* User typedefs */
+
+/* User variables */
+
+/* User functions */
+
+
+
+
+
+
+
+#endif
